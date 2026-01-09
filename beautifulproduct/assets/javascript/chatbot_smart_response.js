4db999fd492212664f28d1d1db2f41f417a73225
anonymousbyhaqi <ditoanan34@gmail.com>
2026-01-09 15:07:58 +0700

    Delete beautifulproduct

--- beautifulproduct/assets/javascript/chatbot_smart_response.js
+++
@@ -1,7 +0,0 @@
-const smartResponses = [
-  "Halo 👋 Saya BeautyBot 💖, saya siap membantu Anda.",
-  "Boleh dijelaskan lebih detail ya? 😊",
-  "Saya bisa membantu rekomendasi produk sesuai jenis kulit Anda 🌸",
-  "Silakan tanyakan seputar produk, cara pakai, atau jenis kulit 💬"
-];
-
